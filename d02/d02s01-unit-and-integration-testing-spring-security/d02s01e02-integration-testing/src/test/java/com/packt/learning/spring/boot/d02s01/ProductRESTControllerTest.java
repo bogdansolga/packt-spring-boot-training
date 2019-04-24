7c24afac108d5f66239282b1c835be5a2bcefed3
Bogdan Solga <bogdan.solga@gmail.com>
2019-04-24 13:40:00 +0300

    [improve] Added the active profile

--- d02/d02s01-unit-and-integration-testing-spring-security/d02s01e02-integration-testing/src/test/java/com/packt/learning/spring/boot/d02s01/ProductRESTControllerTest.java
+++ d02/d02s01-unit-and-integration-testing-spring-security/d02s01e02-integration-testing/src/test/java/com/packt/learning/spring/boot/d02s01/ProductRESTControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,6 +16,7 @@ import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles(Profiles.IN_MEMORY)
 class ProductRESTControllerTest {
 
     @Autowired
